c3ed93a95ed8424bec129366525e7a659bf71ec9
Richard Miller <richardmmill@gmail.com>
2021-05-19 13:05:10 -0400

    beginning App.js, Header.js, Hero.js

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">
+        <p>R</p>
+      </div>
+      <div className="menu">
+        <ul>
+          <li>home</li>
+          <li>about me</li>
+          <li>work</li>
+          <li>contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>Richard Miller Wimmer</h1>
+      <h2>Fullstack Web Developer</h2>
+    </div>
+  );
+}
+
+export default Hero;
