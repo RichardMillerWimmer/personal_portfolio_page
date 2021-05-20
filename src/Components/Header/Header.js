02127c335e075c1333e15585f7999e51703114c6
Richard Miller <richardmmill@gmail.com>
2021-05-20 10:14:32 -0400

    work on landing

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,34 +3,36 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <div className="logo">
-        <p>R</p>
-      </div>
-      <div className="menu">
-        <nav role="navigation">
-          <div id="menuToggle">
-            <input type="checkbox" />
+      <div className="headerContainer">
+        <div className="logo">
+          <p>R</p>
+        </div>
+        <div className="menu">
+          <nav role="navigation">
+            <div id="menuToggle">
+              <input type="checkbox" />
 
-            <span className="span1"></span>
-            <span className="span2"></span>
-            <span className="span3"></span>
+              <span className="span1"></span>
+              <span className="span2"></span>
+              <span className="span3"></span>
 
-            <ul id="menu">
-              <a href="#">
-                <li>home</li>
-              </a>
-              <a href="#">
-                <li>about</li>
-              </a>
-              <a href="#">
-                <li>work</li>
-              </a>
-              <a href="#">
-                <li>contact</li>
-              </a>
-            </ul>
-          </div>
-        </nav>
+              <ul id="menu">
+                <a href="#">
+                  <li>home</li>
+                </a>
+                <a href="#">
+                  <li>about</li>
+                </a>
+                <a href="#">
+                  <li>work</li>
+                </a>
+                <a href="#">
+                  <li>contact</li>
+                </a>
+              </ul>
+            </div>
+          </nav>
+        </div>
       </div>
     </div>
   );
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,7 +3,9 @@ import React from "react";
 function Hero() {
   return (
     <div className="hero">
-      <h1>Richard Miller Wimmer</h1>
+      <h1>
+        Richard<br></br>Miller<br></br>Wimmer
+      </h1>
       <h2>Fullstack Web Developer</h2>
     </div>
   );
