9383568bc5ff7d0d21f399fd0a5554f238ee0832
Richard Miller <richardmmill@gmail.com>
2021-05-20 11:01:48 -0400

    work on landing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./Style/Reset.css";
 import "./Style/Style.css";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="titleContainer">
+        <h4>Richard Miller Wimmer</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
