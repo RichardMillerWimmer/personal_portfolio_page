8275099990815092276c447b81af7acd73b02091
Richard Miller <richardmmill@gmail.com>
2021-05-24 21:58:11 -0400

    trying alice carousel

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,10 +1,30 @@
 import React from "react";
+import AliceCarousel from 'react-alice-carousel';
+import polymath1 from 
+
 
 function Work() {
+
+  const polymathImg = [
+    <img src={} alt=''/>,
+    <img src={} alt=''/>,
+    <img src={} alt=''/>,
+    <img src={} alt=''/>
+  ]
+
+  const ttgImg = [
+    <img src={} alt=''/>,
+    <img src={} alt=''/>,
+    <img src={} alt=''/>,
+    <img src={} alt=''/>
+  ]
+
   return (
     <div className="work">
       <div className="titleContainer">
-        <h4>Work Componenet</h4>
+        <h4>Work</h4>
+        <AliceCarousel></AliceCarousel>
+        <AliceCarousel></AliceCarousel>
       </div>
     </div>
   );
