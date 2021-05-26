e64c2a8a4e239f7a5715192931d00492ff6e541a
Richard Miller <richardmmill@gmail.com>
2021-05-25 21:05:02 -0400

    slide style and button onclicks

--- src/Components/Work/ImageSlider.js
+++ src/Components/Work/ImageSlider.js
@@ -4,11 +4,13 @@ const ImageSlider = (props) => {
     const [current, setCurrent] = useState(0);
 
     const nextSlide = () => {
-
+        setCurrent(current === props.slides.length-1 ? 0 : current+1)
+        console.log(current)
     };
 
     const prevSlide = () => {
-
+        setCurrent(current === 0 ? props.slides.length : current-1)
+        console.log(current)
     };
 
 
@@ -17,7 +19,7 @@ const ImageSlider = (props) => {
         <section className='imageSlider'>
             <p className='prevArrow' onClick={prevSlide}>left</p>
             {props.slides.map((slide, index) => {
-                return <img src={slide} alt=''/>
+                return <img src={slide} alt='' className='image'/> 
             })}
             <p className='nextArrow' onClick={nextSlide}>right</p>
         </section>
