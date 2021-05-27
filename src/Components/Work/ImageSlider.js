7727621a8e161461d188c315d476d29439351ce6
Richard Miller <richardmmill@gmail.com>
2021-05-27 15:18:52 -0400

    work on work section

--- src/Components/Work/ImageSlider.js
+++ src/Components/Work/ImageSlider.js
@@ -1,41 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const ImageSlider = (props) => {
-    const [current, setCurrent] = useState(0);
+  const [current, setCurrent] = useState(0);
 
-    console.log(props.theme)
+  console.log(props.theme);
 
-    const nextSlide = () => {
-        setCurrent(current === props.slides.length-1 ? 0 : current+1)
-    };
+  const nextSlide = () => {
+    setCurrent(current === props.slides.length - 1 ? 0 : current + 1);
+  };
 
-    const prevSlide = () => {
-        setCurrent(current === 0 ? props.slides.length-1 : current-1)
-    };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? props.slides.length - 1 : current - 1);
+  };
 
-
-    console.log(props)
-    return (
-        <div>
-            <h4 className='workTitle'>{props.title}</h4>
-            <section className='imageSlider'>
-                <p className='prevArrow' onClick={prevSlide}>&#10092;</p>
-                <div className='siteHover'>
-                    <a className='imageHover' href={props.site} target='_blank'>
-                    <p className='vistSite'>visit site</p>
-                    {props.slides.map((slide, index) => {
-                        return (
-                            <div className={index === current ? 'slide active' : 'slide'} key={index}>
-                            {current === index && <img src={slide} alt='' className='image'/> }
-                            </div> 
-                        )
-                    })}</a>
+  console.log(props);
+  return (
+    <section>
+      <h4 className="workTitle">{props.title}</h4>
+      <div className="imageSlider">
+        <p className="prevArrow" onClick={prevSlide}>
+          &#10092;
+        </p>
+        <a className="imageHover" href={props.site} target="_blank">
+          <div className="siteHover">
+            {/* <p className="visitSite">visit site</p> */}
+            {props.slides.map((slide, index) => {
+              return (
+                <div
+                  className={index === current ? "slide active" : "slide"}
+                  key={index}
+                >
+                  {current === index && (
+                    <img src={slide} alt="" className="image" />
+                  )}
                 </div>
-                <p className='nextArrow' onClick={nextSlide}>&#10093;</p>
-            
-            </section>
-        </div>
-    )
+              );
+            })}
+          </div>
+        </a>
+        <p className="nextArrow" onClick={nextSlide}>
+          &#10093;
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default ImageSlider;
