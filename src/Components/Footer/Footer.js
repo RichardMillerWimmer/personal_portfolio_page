1c3e832092075565041ca48d23f97f04dd6361e5
Richard Miller <richardmmill@gmail.com>
2021-05-28 10:02:36 -0400

    footer links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+import github from "../../Images/github_icon.png";
+import linkedin from "../../Images/linkedin_icon.png";
+
 function Footer() {
   return (
     <div className="footer">
       <div className="titleContainer">
         <h4>Richard Miller Wimmer</h4>
+        <div>
+          <a href="https://github.com/RichardMillerWimmer" target="_blank">
+            <img className="icon" src={github} alt="github link" />
+          </a>
+          <a href="https://www.linkedin.com/in/richard-miller-wimmer/"
+            target="_blank">
+            <img className="icon" src={linkedin} alt="linkedin link" />
+          </a>
+        </div>
       </div>
     </div>
   );
