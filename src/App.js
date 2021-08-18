4674aac4096c3271845dd80e10bf372b9f071aed
Richard Miller <richardmmill@gmail.com>
2021-08-18 16:04:04 -0400

    adding in rollbar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import routes from "./routes";
 
+import { Provider, ErrorBoundry } from "@rollbar/react";
+
+const rollbarConfig = {
+  accessToken: "17e0ee430c3e407bb547fe1754dbd531",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {routes}
-      <Footer />
-    </div>
+    <Provider config={rollbarConfig}>
+      <div className="App">
+        <Header />
+        {routes}
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
