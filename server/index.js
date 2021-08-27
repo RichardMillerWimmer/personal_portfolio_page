699ea592e0056e44b11eb5d01a4c18a3e003d208
Richard Miller <richardmmill@gmail.com>
2021-08-27 15:35:16 -0400

    clean up in progress

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use(express.static(`${__dirname}/../build`))
 app.use(express.json());
 
 // contorller enpoints
-app.get('/api/work', contoller.getWorks);
-app.get('/api/work/:id', contoller.getWork);
+// app.get('/api/work', contoller.getWorks);
+// app.get('/api/work/:id', contoller.getWork);
 
 
 app.listen(SERVER_PORT, () => console.log(`server is running on ${SERVER_PORT}`));
